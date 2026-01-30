4fb60b6b7ba3c0421fabc048cf495518b4b4a631
Tento <regartento@gmail.com>
2026-01-30 11:17:29 +0700

    add 03_data type

---
+++ src/bin/03_data_type.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let my_int=2;
+    let my_float=2.5;
+    let my_string="Tento";
+    let my_char = 'S';
+    let my_bool=true;
+
+    println!("My int:{}",my_int);
+    println!("My float:{}",my_float);
+    println!("My string:{}",my_string);
+    println!("My char:{}",my_char);
+    println!("My bool:{}",my_bool);
+}
